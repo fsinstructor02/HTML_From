8c5f1ecd16bd27d58bfe7f2e332ae120a0698b63
unknown <fsinstructor02@hussiancollege.edu>
2018-02-28 23:39:22 -0500

    update setup css js index.html

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* this will host js and jq */
+
+var c1 = Math.floor(Math.random() * 60);  
+var c2 = Math.floor(Math.random() * 60);  
+var c3 = Math.floor(Math.random() * 60);  
+var c4 = Math.floor(Math.random() * 60);  
+var c5 = Math.floor(Math.random() * 60);  
+
+
+
+function numberSet() {
+
+
+    // document.getElementById('c1').innerHTML = c1;
+    // document.getElementById('c2').innerHTML = c2;
+    // document.getElementById('c3').innerHTML = c3;
+    // document.getElementById('c4').innerHTML = c4;
+    // document.getElementById('c5').innerHTML = c5;
+
+    
+}
